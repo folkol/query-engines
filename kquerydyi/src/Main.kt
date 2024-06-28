91c7f487167e815c1141e612914319bdb06d1de9
Matte <mattias4@kth.se>
2024-06-28 22:34:49 +0200

    Added Scan

--- kquerydyi/src/Main.kt
+++ kquerydyi/src/Main.kt
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.DatabindException
 import com.sun.jdi.BooleanType
 import org.apache.arrow.vector.types.FloatingPointPrecision
 import org.apache.arrow.vector.types.pojo.ArrowType
@@ -208,6 +209,40 @@ class Count(input: LogicalExpr) : AggregateExpr("COUNT", input) {
         return "COUNT($expr)"
     }
 }
+
+class Scan(
+    val path: String,
+    val dataSource: DataSource,
+    val projection: List<String>
+) : LogicalPlan {
+    val schema = deriveSchema()
+
+    override fun schema(): Schema {
+        return schema
+    }
+
+    private fun deriveSchema(): Schema {
+        val schema = dataSource.schema()
+        if (projection.isEmpty()) {
+            return schema
+        } else {
+            return schema.select(projection)
+        }
+    }
+
+    override fun children(): List<LogicalPlan> {
+        return listOf()
+    }
+
+    override fun toString(): String {
+        return if (projection.isEmpty()) {
+            "Scan: $path; projection=None"
+        } else {
+            "Scan: $path; projection=$projection"
+        }
+    }
+}
+
 
 
 
