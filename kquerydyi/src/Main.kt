9338c63e71db327e102a5f57d82ecde71474e9df
Matte <mattias4@kth.se>
2024-06-28 22:37:04 +0200

    Added Projection

--- kquerydyi/src/Main.kt
+++ kquerydyi/src/Main.kt
@@ -243,6 +243,26 @@ class Scan(
     }
 }
 
+class Projection(
+    val input: LogicalPlan,
+    val expr: List<LogicalExpr>
+) : LogicalPlan {
+    override fun schema(): Schema {
+        return Schema(expr.map { it.toField(input) })
+    }
+
+    override fun children(): List<LogicalPlan> {
+        return listOf(input)
+    }
+
+    override fun toString(): String {
+        return "Projection: ${
+            expr.map {
+                it.toString()
+            }.joinToString(",")
+        }"
+    }
+}
 
 
 
