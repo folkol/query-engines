b7813ea99884aca2d6c96c2f61d3e6e0e9a84bd7
Matte <mattias4@kth.se>
2024-06-28 21:54:06 +0200

    Added ArrowTypes and ColumnVector interface

---
+++ kquerydyi/src/Main.kt
@@ -0,0 +1,27 @@
+import org.apache.arrow.vector.types.FloatingPointPrecision
+import org.apache.arrow.vector.types.pojo.ArrowType
+
+object ArrowTypes {
+    val BooleanType = ArrowType.Bool()
+    val Int8Type = ArrowType.Int(8, true)
+    val Int16Type = ArrowType.Int(16, true)
+    val Int32Type = ArrowType.Int(32, true)
+    val Int64Type = ArrowType.Int(64, true)
+    val UInt8Type = ArrowType.Int(8, false)
+    val UInt16Type = ArrowType.Int(16, false)
+    val UInt32Type = ArrowType.Int(32, false)
+    val UInt64Type = ArrowType.Int(64, false)
+    val FloatType = ArrowType.FloatingPoint(FloatingPointPrecision.SINGLE)
+    val DoubleType = ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)
+    val StringType = ArrowType.Utf8()
+}
+
+interface ColumnVector {
+    fun getType(): ArrowType
+    fun getValue(i: Int): Any?
+    fun size(): Int
+}
+
+fun main() {
+    println("Lol")
+}
